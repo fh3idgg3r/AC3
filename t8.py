1f79cc79452680bac707eb0979fdd8cbed777271
fh3idgg3r <olhaoleg@outlook.com.br>
2020-10-10 14:05:39 -0300

    AC

--- t8.py
+++ t8.py
@@ -12,4 +12,4 @@ def ac3_defesa_cibernetica():
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
-	app.run(host = '192.168.56.1', port = port)
+	app.run(host = '0.0.0.0', port = port)
