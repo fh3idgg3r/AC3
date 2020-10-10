772713fb46ffc71106dcbbe30da4e859edde6dab
fh3idgg3r <olhaoleg@outlook.com.br>
2020-10-10 15:24:19 -0300

    AC

--- t8.py
+++ t8.py
@@ -1,15 +1,13 @@
-import os
-from flask import Flask, jsonify, request
-from math import sqrt
+limite = 600
+c = 1
+p = 0
 
-app = Flask(__name__)
+for numero in range(2, limite+1):
+    for i in range(2,numero):
+        if numero % i == 0: break
+        c += 1
+    else:
+        print (numero,)
+        p += 1
 
-@app.route('/')
-def ac3_defesa_cibernetica():
-
-	primos = "tudo vai dar certo"
-	return primos
-
-if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host = '0.0.0.0', port = port)
+return ("\n\nEncontramos %d números primos hihi." %p)
