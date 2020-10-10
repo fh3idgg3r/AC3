eb7ccddd3941fc0f8813de720bcb335507de6741
fh3idgg3r <olhaoleg@outlook.com.br>
2020-10-10 15:27:39 -0300

    AC

--- t8.py
+++ t8.py
@@ -1,3 +1,7 @@
+import os
+
+app = Flask(__name__)
+
 limite = 600
 c = 1
 p = 0
@@ -11,3 +15,8 @@ for numero in range(2, limite+1):
         p += 1
 
 return ("\n\nEncontramos %d números primos hihi." %p)
+
+if __name__ = "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
+
