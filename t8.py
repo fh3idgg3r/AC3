a85e1115702c668eadffabf7826ca44907b154e8
fh3idgg3r <olhaoleg@outlook.com.br>
2020-10-10 16:44:16 -0300

    AC3

--- t8.py
+++ t8.py
@@ -2,23 +2,25 @@ import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
 @app.route('/ac3')
 
-limite = 600
-c = 1
-p = 0
+def ac3_defesa():
+     limite = 600
+     c = 1
+     p = 0
 
-for numero in range(2, limite+1):
-    for i in range(2,numero):
-        if numero % i == 0: break
-        c += 1
-    else:
-        print (numero,)
-        p += 1
+     for numero in range(2, limite+1):
+         for i in range(2,numero):
+             if numero % i == 0: break
+             c += 1
+         else:
+             return (numero,)
+             p += 1
 
-return ("\n\nEncontramos %d números primos hihi." %p)
-
-if __name__ = "__main__":
+     return ("\n\nEncontramos %d numeros primos hihi." %p)
+	
+if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
 
